0f7f8ec394b61d7e072ace18561c9dce868bdba2
Abdullahi Akorede <aabdullahiakorede@gmail.com>
2026-05-08 11:05:12 -0700

    style: apply clean theme and active link states

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,8 @@
 <template>
   <div id="app">
     <nav class="navbar">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link> |
+      <router-link to="/">Home</router-link> 
+      <router-link to="/about">About</router-link> 
       <router-link to="/contact">Contact</router-link>
     </nav>
 
